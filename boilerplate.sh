f7df29b6ad28e097d186e4d42a50ca55d1c288e1
Ranadeep Polavarapu <ranadeep@icloud.com>
2015-03-01 12:07:30 -0500

    docs: Init superuser mode docs added

--- boilerplate.sh
+++ boilerplate.sh
@@ -56,8 +56,9 @@ FS_UL="4m"
 
 
 
-
-# Perform an initial sudo command by taking superuser password as argv at location 1
+# --- Initialize SUPERUSER mode ---
+# Perform an initial sudo command by taking superuser password as argv at location 1.
+# This will allow this shell script to perform operations with sudo without password prompt every time.
 echo ${1} | sudo -S ls
 
 sudo apt-get -y update
