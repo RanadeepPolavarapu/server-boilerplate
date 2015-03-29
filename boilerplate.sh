3a8b1c3a911700e86fe54d58ebdb0adc1f393eef
Ranadeep Polavarapu <ranadeep@icloud.com>
2015-03-28 21:34:31 -0400

    Disable output from boilerplate.sh apt-get

--- boilerplate.sh
+++ boilerplate.sh
@@ -39,7 +39,7 @@
 # This will allow this shell script to perform operations with sudo without password prompt every time.
 echo ${1} | sudo -S ls
 
-sudo apt-get -y update
+sudo apt-get -y update 1> /dev/null
 echo -e "[INFO]: | $(date) | Performed apt-get update."
-sudo apt-get -y upgrade
+sudo apt-get -y upgrade 1> /dev/null
 echo -e "[INFO]: $(date) - Performed apt-get upgrade."
